9b0acdada851e53f2f15dc2a3a007452a23457bb
Clément Bouillier <clement.bouillier@devcrafting.com>
2019-04-01 09:17:22 +0200

    Add Golden master

---
+++ csharp/LiftPassPricingTests/LiftServicesShould.cs
@@ -0,0 +1,48 @@
+using System;
+using MySql.Data.MySqlClient;
+using Xunit;
+using FsCheck.Xunit;
+using FluentAssertions;
+using System.Linq;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+
+public class LiftServicesShould
+{
+    private readonly LiftServices liftServices;
+
+    public Random random { get; }
+
+    public LiftServicesShould()
+    {
+        var connection = new MySqlConnection
+            {
+                ConnectionString = @"Database=lift_pass;Data Source=localhost;User Id=root;Password=mysql"
+            };
+        connection.Open();
+        liftServices = new LiftServices(connection);
+        random = new Random(1); 
+    }
+
+    [Fact]
+    [UseReporter(typeof(DiffReporter))]
+    public void ReturnGoldenMaster()
+    {
+        var types = new [] { "night", "1jour" };
+
+        var result =
+            Enumerable.Range(0, 99)
+                .Cast<int?>()
+                .Concat(new int?[] { null })
+                .SelectMany(age => types.SelectMany(type => {
+                    var date = new DateTime(random.Next(2017, 2019), random.Next(1, 12), random.Next(1, 28)).ToString("yyyy-MM-dd");
+                    return new [] {
+                        $"- {age}, {type}, {date}\n",
+                        liftServices.GetPrice(age, type, date) + "\n",
+                        liftServices.GetPrice(age, type, "2019-02-18") + "\n"
+                    };
+                }));
+
+        Approvals.Verify(string.Concat(result));
+    }
+}
